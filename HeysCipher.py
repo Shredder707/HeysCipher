fd47f8e77d89fb50cd17cb0aa39c3d6654502999
ShredderEatsChedder <86796538+Shredder707@users.noreply.github.com>
2024-06-02 18:41:43 +0100

    Add files via upload

---
+++ HeysCipher.py
@@ -0,0 +1,9 @@
+import string
+alphabets = list(string.ascii_letters)  
+print(alphabets)  
+
+def heysCipherEncrypt():
+    print("Encrypts plaintext to ciphertet using heys cipher")
+
+def heysCipherDecrypt():
+    print("Decrypts ciphertet to plaintext using heys cipher")
